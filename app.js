aa3c1ae6c3cc9eccce31084cbfdc3b975120ffd8
Hector Rendon <hrchilpo@gmail.com>
2021-10-05 15:27:21 -0500

    Solucionado el puertpo

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/public"));
 
